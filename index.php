8b14d319e2936aaa6d8236b38bf78025df68b8dd
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-24 15:45:24 +0300

    added link to home

--- index.php
+++ index.php
@@ -16,8 +16,9 @@ get_header();
                 <nav class="nav">
                     <ul class="nav-group-list">
                         <li class="nav-item active">
-                            <a href="index.html" class="nav-link">It services</a>
+                            <a href="<?php echo home_url() ?>" class="nav-link">It services</a>
                         </li>
+<!--                        TODO add link to consulting-eng-->
                         <li class="nav-item">
                             <a href="consulting-eng.html" class="nav-link">Consulting</a>
                         </li>
