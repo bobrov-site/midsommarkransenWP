6401821cff5f5f72a64efd51b8482bb097dfa09e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-29 11:51:36 +0300

    added acf to consulting

--- page-86.php
+++ page-86.php
@@ -1,7 +1,12 @@
 <?php
 get_header();
 ?>
+<?php
+$assignments = get_field('recent_roles_and_assignments_group');
 
+$companyInformation = get_field('company_information_group');
+$companyInformationItem = $companyInformation['fourth_field'];
+?>
 <div class="consulting">
 	<div class="hero-screen">
 		<div class="wrapper">
@@ -48,15 +53,15 @@ get_header();
 		<div class="wrapper">
 			<div class="hero-screen-content">
 				<h1 class="hero-screen-title">
-					Consulting
+					<?php the_field('hero_title'); ?>
 				</h1>
 				<p class="hero-screen-description">
-					When excellence and dedication matters.
+					<?php the_field('hero_description'); ?>
 				</p>
 			</div>
 		</div>
 		<div class="hero-screen-provide">
-			Do you need the key players on your team that make all the difference? For these purposes we offer our senior consultant   Mats Blomdahl, a seasoned specialist in software development, DevOps, Configuration Management, and Site Reliability. He's not sick, does not give up, and always cheerful about his passions in life, "truth and (engineering) beauty".
+			<?php the_field('long_description'); ?>
 		</div>
 	</div>
 	<main>
@@ -66,28 +71,28 @@ get_header();
 			</div>
 			<div class="info recent-roles">
 				<h2 class="info-title">
-					Recent roles and assignments:
+					<?php the_field('recent_roles_and_assignments_title'); ?>
 				</h2>
 				<ul class="info-group-list">
 					<li class="info-item">
-						<span>Pega DevOps at the major Swedish insurance company</span>
+						<span><?php echo $assignments['first_field'] ?></span>
 					</li>
 					<li class="info-item">
-						<span>Software Engineer at Swedish Royal Library, OAuth2 provider</span>
+						<span><?php echo $assignments['second_field'] ?></span>
 					</li>
 					<li class="info-item">
                     <span>
-                        AI division DevOps Load at Sweden's biggest retail chain
+                        <?php echo $assignments['third_field'] ?>
                     </span>
 					</li>
 					<li class="info-item">
                     <span>
-                        CI/CD infra automation & maintenance for big US company
+                        <?php echo $assignments['fourth_field'] ?>
                     </span>
 					</li>
 					<li class="info-item">
                     <span>
-                        Configuration Manager at Swedish gov't high-profile project
+                        <?php echo $assignments['fifth_field'] ?>
                     </span>
 					</li>
 				</ul>
@@ -99,31 +104,34 @@ get_header();
 					GitHub
 				</h2>
 				<p class="info-text">
-					Please find us on GitHub for various open-source projects,
+					<?php the_field('github_text'); ?>
 				</p>
 				<a href="https://github.com/mkdevops-se/" class="info-text info-item-link">
 					https://github.com/mkdevops-se/
 				</a>
 			</div>
-			<div class="info company-information">
-				<h2 class="info-title">
-					Company information:
-				</h2>
-				<ul class="info-group-list">
-					<li class="info-item">
-						<span>Visiting address: Herserudsvägen 18, Lidingö</span>
-					</li>
-					<li class="info-item">
-						<span>Organisation number: 559114-7532</span>
-					</li>
-					<li class="info-item">
-						<span>F-tax and VAT: Yes \</span>
-					</li>
-				</ul>
-				<p class="info-text">
-					Request a quote at <a class="info-item-link mail" href="mailto:sales@mkdevops.se">sales@mkdevops.se</a> or <a class="info-item-link phone" href="tel:+460730567567">+46 (0) 730-567 567</a>.
-				</p>
-			</div>
+            <div class="info company-information">
+                <h2 class="info-title">
+					<?php the_field('company_information_title'); ?>
+                </h2>
+                <ul class="info-group-list">
+                    <li class="info-item">
+                        <span><?php echo $companyInformation['first_field']; ?></span>
+                    </li>
+                    <li class="info-item">
+                        <span><?php echo $companyInformation['second_field'] ?></span>
+                    </li>
+                    <li class="info-item">
+                        <span><?php echo $companyInformation['third_field'] ?></span>
+                    </li>
+                </ul>
+                <p class="info-text">
+					<?php echo $companyInformationItem['fourth_field_1'] ?>
+                    <a class="info-item-link mail" href="<?php echo 'mailto:' . $companyInformationItem['fourth_field_mail'] ?>"><?php echo $companyInformationItem['fourth_field_mail'] ?></a>
+					<?php echo $companyInformationItem['fourth_field_2'] ?>
+                    <a class="info-item-link phone" href="<?php echo 'tel:' . $companyInformationItem['fourth_field_phone'] ?>"><?php echo $companyInformationItem['fourth_field_phone'] ?></a>.
+                </p>
+            </div>
 			<div class="logo-bg"></div>
 		</div>
 	</main>
